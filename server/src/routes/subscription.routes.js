7853b2eeee2e7906ea64404e176b13a38f214148
zak20021380 <chatgpt20021380@gmail.com>
2025-10-08 13:56:08 +0330

    Fix subscription me route

--- server/src/routes/subscription.routes.js
+++ server/src/routes/subscription.routes.js
@@ -3,5 +3,6 @@ const { protect } = require('../middleware/auth');
 const subscriptionController = require('../controllers/subscription.controller');
 
 router.get('/', protect, subscriptionController.getSubscriptionStatus);
+router.get('/me', protect, subscriptionController.getSubscriptionStatus);
 
 module.exports = router;
